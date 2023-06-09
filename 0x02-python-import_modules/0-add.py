6389e2200c462d6ba6d9c577bdaf219e03f73d4f
aitkazbi <khadija.aitkazbi@gmail.com>
2023-06-09 01:16:31 +0100

    Tasck 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+a = 1
+b = 2
+from add_0 import add
 
-if __name__ == "__main__":
-    """Print la somme of 1 and 2."""
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
