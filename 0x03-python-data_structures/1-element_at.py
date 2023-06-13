2056f96130ebb37c7b6b452434f73d0b3be98ef4
aitkazbi <khadija.aitkazbi@gmail.com>
2023-06-13 23:49:36 +0000

    tasck 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for idx in range(len(my_list)):
-        if my_list[idx] < 0 or idx >= len(my_list):
+ for idx in range(len(my_list)):
+        if my_list[idx] < 0 or idx > len(my_list):
         return None
+ else:
+        return my_list[idx]
