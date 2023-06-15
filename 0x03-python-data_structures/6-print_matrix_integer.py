541373d17f29b65d367b6d94933e6e34b807bf52
aitkazbi <khadija.aitkazbi@gmail.com>
2023-06-15 14:24:44 -0700

    Tasck 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for l in matrix:
+        for i in range(len(l)):
+            print("{:d}".format(l[i]), end="")
+            if i != len(l) - 1:
+                print(" ", end="")
+        print()
