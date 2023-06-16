e7fc4d9e3789cb25ec6c23237d97581e7d87552b
aitkazbi <khadija.aitkazbi@gmail.com>
2023-06-16 21:08:26 +0100

    Tack 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+def max_integer(my_list=[]):
+    """Return the biggest integer of a list."""
+    if len(my_list) == 0:
+        return None
+
+    maxnum = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > maxnum:
+            maxnum = my_list[i]
+    return maxnum
