598250684e56bc2aadf2cd5c98bfa80b40d4918d
aitkazbi <khadija.aitkazbi@gmail.com>
2023-06-27 01:58:31 +0100

    Tasck 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     total = 0
     try:
@@ -5,7 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(f"{my_list[i]}", end="")
             total += 1
     except IndexError:
-        break
-
-    print()  
+    
+         print()  
     return total
