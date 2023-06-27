c4f9c48c3cf8ede6fc1a3924ee4d57881ea78fa1
aitkazbi <khadija.aitkazbi@gmail.com>
2023-06-27 13:51:02 +0100

    Tasck 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
+from decimal import DivisionUndefined
+
+
 def safe_print_division(a, b):
     try:
-        div = a / b
+        division = a / b
     except (ZeroDivisionError, FloatingPointError):
         
-        div = None
+        division = None
         
     finally:
-       print("Inside result: {}".format(div))
-       return div
+       print("Inside result: {}".format(division))
+       return division
