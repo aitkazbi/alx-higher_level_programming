833ae4fa4f09da1a9a605ba440c71cceaa1c7d34
aitkazbi <khadija.aitkazbi@gmail.com>
2023-06-28 13:30:22 +0100

    Tasck 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Definit une class Square."""
+
+
+class Square:
+    """Represent a square."""
+    
+    
+    def __init__(self, size):
+        """Initialiser un nouveau Square.
+
+        Args:
+            size (int): The size du nouveux square.
+        """
+        self.__size = size
