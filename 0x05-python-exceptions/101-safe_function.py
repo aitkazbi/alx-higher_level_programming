71b771b6e3f201816c48d9883357f9bb71989c45
aitkazbi <khadija.aitkazbi@gmail.com>
2023-06-29 01:01:14 +0100

    Tasck 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,10 +4,10 @@ import sys
 
 
 def safe_function(fct, *args):
- 
+
     try:
         result = fct(*args)
         return (result)
-    except:
+    except(TypeError, ValueError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
