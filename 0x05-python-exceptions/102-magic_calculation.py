97a17260473cbbe78be1e09c101e4672fdf9bf03
aitkazbi <khadija.aitkazbi@gmail.com>
2023-06-29 01:31:42 +0100

    Tasck 9

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+import sys
 
 
 def magic_calculation(a, b):
     result = 0
-
-    for i in range(1, 4):
+    for i in range(1, 3):
         try:
             if i > a:
-                raise ValueError('Too far')
-            result += (a ** b) / i
+                raise Exception("Too far")
+            else:
+                result += (a**b) / i
         except Exception as e:
             result = b + a
             print("Exception: {}".format(e), file=sys.stderr)
             break
-
     return result
